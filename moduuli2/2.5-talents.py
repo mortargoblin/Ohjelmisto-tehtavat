5a4909a8badc8c93c69e06e1efde1037880a6ae3
mortargoblin <aaro.euro@gmail.com>
2025-01-16 10:06:50 +0200

    bugikorjauksia 2.5 -tehtävässä

--- moduuli2/2.5-talents.py
+++ moduuli2/2.5-talents.py
@@ -2,14 +2,14 @@ print("Enter talents, pounds, lots to convert to modern units")
 
 try:
     talents = float(input("talents: "))
-    punds = float(input("punds: "))
+    pounds = float(input("pounds: "))
     lots = float(input("lots: "))
 except ValueError:
     print("Input not float!")
     exit()
 
 # varmaan löytyy helpompi tapa tehdä tämä......
-result = (lots * 13.5 + punds * 425.6 + talents * 8512)/100
+result = (lots * 13.5 + pounds * 425.6 + talents * 8512)/1000
 mu = str(result).split(".")
 
 print(f"The weight in modern units: {mu[0]}Kg, {mu[1].strip("0")}g")
