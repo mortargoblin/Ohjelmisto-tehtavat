3245d9ef6ce182e09273390cfd30e5453823b2ea
mortargoblin <aaro.euro@gmail.com>
2025-01-18 14:32:42 +0200

    moduuli4 valmis

---
+++ moduuli4/4.6-pi.py
@@ -0,0 +1,23 @@
+# pi-algorytmi
+
+def main():
+    print("Enter number of points")
+    n = int(input("> "))
+    print("Pi approximately equals:", calc_pi(n))
+
+def calc_pi(points):
+    from random import uniform
+    from math import pi
+
+    # tässä kohtaa arvotaan pisteiden sijainnit ja lasketaan osumat
+    hit = 0
+    for _ in range(points):
+        x = uniform(-1,1)
+        y = uniform(-1,1)
+        if pow(x,2) + pow(y,2) < 1:
+            hit += 1
+        else:
+            pass
+    return 4 * hit / points
+
+main()
