dc9ca5a8ff5143edd5673f5fba5a9d2abd473d01
mortargoblin <aaro.euro@gmail.com>
2025-03-19 13:02:39 +0200

    9.4 viimeistelty

--- ohjelmisto-2/moduuli9/9.4-kilpailu.py
+++ ohjelmisto-2/moduuli9/9.4-kilpailu.py
@@ -18,23 +18,28 @@ class Auto:
     def kulje(self, tunnit):
         self.kulj_matka += self.nyk_nopeus * tunnit
 
+# Autot määritetään
 autot = []
 for i in range(10):
     uusi_auto = Auto(f"ABC-{i+1}", randint(100, 200))
     autot.append(uusi_auto)
 
 # Kilpailu alkaa
-maali_saavutettu = False
-while maali_saavutettu == False:
+while True:
     for auto in autot:
         auto.kiihdytä(randint(-15,15))
         auto.kulje(1)
         if auto.kulj_matka >= 10000:
-            maali_saavutettu = True
+            break
+    else:
+        continue
+    break
 
 # Selkeä taulukko:
 print(f"""+-----------+--------------+------------------+
 | Rekkari   | Huippunopeus | Kuljettu matka   |""")
 for auto in autot:
     print(f"""+-----------+--------------+------------------+
-| {auto.rekkari.ljust(6)}    | {auto.huip_nopeus}          | {(str(auto.kulj_matka)+"km").ljust(17)}|""")
+| {auto.rekkari.ljust(6)}    | {auto.huip_nopeus}          | """
+f"{(str(auto.kulj_matka)+"km").ljust(17)}|")
+print("+-----------+--------------+------------------+")
