5f3d7d102b9f614e3403b743e9b33d02bb9ae3b5
mortargoblin <aaro.euro@gmail.com>
2025-04-10 15:36:51 +0300

    joo

--- ohjelmisto-2/moduuli13/13.2-airport.py
+++ ohjelmisto-2/moduuli13/13.2-airport.py
@@ -3,37 +3,34 @@ import mysql.connector
 import json
 
 app = Flask(__name__)
-@app.route("/alkuluku/<num>")
+@app.route("/kentta/<icao>")
 
-yhteys = mysql.connector.connect (
-    host='127.0.0.1',
-    port=3306,
-    database='flight_game',
-    user='pythonuser',
-    autocommit=True,
-    collation='utf8mb3_general_ci'
-)
 
-def prime_num_check(num):
+
+def get_airport(icao):
+    yhteys = mysql.connector.connect (
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='pythonuser',
+        password='salainen-sana',
+        autocommit=True,
+        collation='utf8mb3_general_ci'
+    )
+    kursori = yhteys.cursor(dictionary=True)
     try:
-        num = int(num)
         tilakoodi = 200
-        vastaus = {
-            "status": tilakoodi,
-            "number": num, 
-            "isPrime": None}
-        for i in range(2, num -1):
-            if num % i == 0:
-                vastaus["isPrime"] = False
-                break
-        if vastaus["isPrime"] == None:
-            vastaus["isPrime"] = True
-
-    except ValueError:
-        tilakoodi = 400
-        vastaus = {
-            "status": tilakoodi,
-            "description": "Improper number"}
+        kursori.execute(
+            f"SELECT ident AS ICAO, " 
+            f"name AS Name, "
+            f"municipality AS Municipality "
+            f"FROM airport "
+            f"WHERE ident='{icao}'"
+        )
+        vastaus = kursori.fetchone()
+    except Exception as e:
+        vastaus = e
+#    return str(vastaus)
     return Response(
         response=json.dumps(vastaus), 
         status=tilakoodi, 
@@ -44,7 +41,8 @@ def prime_num_check(num):
 def page_not_found(virhekoodi):
     vastaus = {
         "status": 404,
-        "description": "Improper endpoint"}
+        "description": "Improper endpoint"
+    }
 
     return Response(response=json.dumps(vastaus), status=404, mimetype="application/json")
 
